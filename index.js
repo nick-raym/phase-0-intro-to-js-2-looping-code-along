ae6715a76eaa308de7fc34ea64b523f3cbb5678e
NickRayman <nlhrayman@gmail.com>
2023-12-18 01:13:17 -0500

    complete loops

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+/* Code your solutions in this file
+for(let age = 30; age < 40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);*/
+
+
+function writeCards(names, eventName){
+    const completeMessages = [];
+    for(let i=0;i<names.length;i++){
+        let message=`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`
+        completeMessages.push(message);
+    }
+    return completeMessages;
+}
+
+function countDown(n){
+    while(n>=0){
+        console.log(n);
+        n--
+    }
+}
